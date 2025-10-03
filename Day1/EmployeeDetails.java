603b187593ef64fc67d81108e5e95705e21ac3a7
Preethi57 <83569559+Preethi57@users.noreply.github.com>
2025-10-03 19:35:10 +0530

    Add files via upload
    day1

---
+++ Day1/EmployeeDetails.java
@@ -0,0 +1,41 @@
+package week1.Day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name="Preethi";
+		short age= 25;
+		double salary=55000.75;
+		char gender='F';
+		boolean accept_terms=true;
+		float rating=4.7f;
+	    long mobile_no=986543210l;
+		int pincode=56001;
+		byte luckyNumber=7;
+		
+		
+        System.out.println("Name : "+ name);
+        System.out.println("age : "+ age);
+        System.out.println("Salary : "+ salary);
+        System.out.println("Gender: "+ gender);
+        System.out.println("Accept terms : "+ accept_terms);
+        System.out.println("Rating : "+ rating);
+        System.out.println("Phone number : "+ mobile_no);
+        System.out.println("Pincode : "+ pincode);
+        System.out.println("lucky Number : "+ luckyNumber);
+        
+        
+        
+        
+        
+        
+        
+        
+		
+		
+		
+
+	}
+
+}
