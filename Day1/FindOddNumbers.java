6a1244397658cea5390889c1aeb6972e5ee2d5f1
Preethi57 <83569559+Preethi57@users.noreply.github.com>
2025-10-07 09:39:09 +0530

    Add files via upload
    Week2/Day1

---
+++ Day1/FindOddNumbers.java
@@ -0,0 +1,22 @@
+package week2.Day1;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		int num=10;
+		
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2!=0) {
+				System.out.println(i);
+		}
+		}
+		
+		
+	
+			
+		
+
+	}
+
+}
